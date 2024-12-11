936a4f22a6df03bed2a3e92b8340678e87663478
patrictirri <patrictirri@gmail.com>
2024-12-11 22:47:03 +0200

    Fix template

--- src/advent_of_code_2024/days/template.py
+++ src/advent_of_code_2024/days/template.py
@@ -1,26 +1,35 @@
 import os
 
-INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
-with open(INPUT) as f:
-    data = f.read()
+
+def main():
+    INPUT = os.path.join(os.path.dirname(__file__), "input.txt")
+    with open(INPUT) as f:
+        data = f.read()
+
+    test = """"""
+
+    print(part_one(test, True))
+    print(part_one(data))
+
+    # Part 2 test
+    # test = """"""
+
+    print(part_two(test, True))
+    print(part_two(data))
 
 
-def part_one(data: str):
+def print_part(part: int, test_run: bool = False):
+    string = f"Part {part}"
+    if test_run:
+        string += " (test)"
+    print(string)
+
+
+def part_one(data: str, test_run: bool = False):
+    print_part(1, test_run)
     pass
 
 
-test = """"""
-
-print(part_one(test))
-print(part_one(data))
-
-
-def part_two(data: str):
+def part_two(data: str, test_run: bool = False):
+    print_part(2, test_run)
     pass
-
-
-# Part 2 test
-# test = """"""
-
-print(part_two(test))
-print(part_two(data))
